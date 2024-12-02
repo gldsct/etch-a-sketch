c1e723a663176b863b0d70338a50418e768c18d3
gldsct <188839581+gldsct@users.noreply.github.com>
2024-12-02 13:18:47 +0545

    Add custom grid size functionality within page
    Before, a prompt was shown to select grid size.
    It was replaced with "input" and "button" elements
    within the page for a better user experience. The
    webpage is now also capable of changing grid size
    dynamically (without refreshing the webpage).

--- javascript.js
+++ javascript.js
@@ -1,17 +1,65 @@
-let size;
+let gridSize;
 
-while (true) {
-    size = prompt("Enter a size between 1 and 100.");
-    if (size <= 100 && size > 0) {
-        break;
-    }
-}
+// while (true) {
+//     size = prompt("Enter a size between 1 and 100.");
+//     if (size <= 100 && size > 0) {
+//         break;
+//     }
+// }
 
 gridHeader = document.querySelector(".grid-header");
 
 gridContainer = document.querySelector(".grid-container");
 
-gridHeader.innerText = `${size} x ${size} Square Grid`;
+gridSizeInput = document.querySelector("#grid-size-input");
+gridSizeButton = document.querySelector(".grid-size-button");
+
+gridHeader.innerText = `Square Grid`;
+
+gridSizeButton.addEventListener("click", (event) => {
+    if (gridContainer.hasChildNodes()) {
+        gridSemiContainers.forEach(item => {
+            gridContainer.removeChild(item);
+        });
+    }
+   gridSize = gridSizeInput.value;
+    gridHeader.innerText = `${gridSize} x ${gridSize} Square Grid`;
+
+    for (i = 0; i <gridSize; i ++) {
+        gridSemiContainer = document.createElement("div");
+        gridSemiContainer.className = "grid-semi-container";
+        gridSemiContainer.style.height = `${560/gridSize}px`;
+        gridSemiContainer.style.width = "560px";
+        gridSemiContainer.style.backgroundColor = "#CFCFCD";
+        gridSemiContainer.style.border = "1px solid #563440";
+        gridContainer.appendChild(gridSemiContainer);
+    }
+    
+    gridSemiContainers = document.querySelectorAll(".grid-semi-container");
+    gridSemiContainers.forEach(item => {
+        item.style.display = "flex";
+    });
+    
+    gridSemiContainers.forEach(item => {
+        for (i = 0; i <gridSize; i ++) {
+        gridSquare = document.createElement("div");
+        gridSquare.className = "grid-square";
+        gridSquare.style.width = `${560/gridSize}px`;
+        gridSquare.style.height = `${560/gridSize}px`;
+        gridSquare.style.backgroundColor = "#CFCFCD";
+        gridSquare.style.border = "1px solid #563440";
+        item.appendChild(gridSquare);  
+        }
+    });
+    
+    gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach(item => {
+        item.addEventListener("mouseenter", (event) => {
+            event.currentTarget.style.backgroundColor = "orange";
+        });
+    });
+
+});
 
 // gridSquare = document.createElement("div");
 // gridSquare.style.width = "35px";
@@ -30,39 +78,7 @@ gridHeader.innerText = `${size} x ${size} Square Grid`;
 //     gridContainer.appendChild(gridSquare);
 // }
 
-for (i = 0; i < size; i ++) {
-    gridSemiContainer = document.createElement("div");
-    gridSemiContainer.className = "grid-semi-container";
-    gridSemiContainer.style.height = `${560/size}px`;
-    gridSemiContainer.style.width = "560px";
-    gridSemiContainer.style.backgroundColor = "#CFCFCD";
-    gridSemiContainer.style.border = "1px solid #563440";
-    gridContainer.appendChild(gridSemiContainer);
-}
 
-gridSemiContainers = document.querySelectorAll(".grid-semi-container");
-gridSemiContainers.forEach(item => {
-    item.style.display = "flex";
-});
-
-gridSemiContainers.forEach(item => {
-    for (i = 0; i < size; i ++) {
-    gridSquare = document.createElement("div");
-    gridSquare.className = "grid-square";
-    gridSquare.style.width = `${560/size}px`;
-    gridSquare.style.height = `${560/size}px`;
-    gridSquare.style.backgroundColor = "#CFCFCD";
-    gridSquare.style.border = "1px solid #563440";
-    item.appendChild(gridSquare);  
-    }
-});
-
-gridSquares = document.querySelectorAll(".grid-square");
-gridSquares.forEach(item => {
-    item.addEventListener("mouseenter", (event) => {
-        event.currentTarget.style.backgroundColor = "orange";
-    });
-});
 
 
 
