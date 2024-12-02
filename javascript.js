ff4861738d0b287d7b8ba08f67e399b4aded907f
gldsct <188839581+gldsct@users.noreply.github.com>
2024-12-02 12:00:37 +0545

    Add prompt to select grid size

--- javascript.js
+++ javascript.js
@@ -1,5 +1,14 @@
 gridContainer = document.querySelector(".grid-container");
 
+let size;
+
+while (true) {
+    size = prompt("Enter a size between 1 and 100.");
+    if (size <= 100 && size > 0) {
+        break;
+    }
+}
+
 // gridSquare = document.createElement("div");
 // gridSquare.style.width = "35px";
 // gridSquare.style.height = "35px";
@@ -17,10 +26,10 @@ gridContainer = document.querySelector(".grid-container");
 //     gridContainer.appendChild(gridSquare);
 // }
 
-for (i = 0; i < 16; i ++) {
+for (i = 0; i < size; i ++) {
     gridSemiContainer = document.createElement("div");
     gridSemiContainer.className = "grid-semi-container";
-    gridSemiContainer.style.height = "35px";
+    gridSemiContainer.style.height = `${560/size}px`;
     gridSemiContainer.style.width = "560px";
     gridSemiContainer.style.backgroundColor = "#CFCFCD";
     gridSemiContainer.style.border = "1px solid #563440";
@@ -33,11 +42,11 @@ gridSemiContainers.forEach(item => {
 });
 
 gridSemiContainers.forEach(item => {
-    for (i = 0; i < 16; i ++) {
+    for (i = 0; i < size; i ++) {
     gridSquare = document.createElement("div");
     gridSquare.className = "grid-square";
-    gridSquare.style.width = "35px";
-    gridSquare.style.height = "35px";
+    gridSquare.style.width = `${560/size}px`;
+    gridSquare.style.height = `${560/size}px`;
     gridSquare.style.backgroundColor = "#CFCFCD";
     gridSquare.style.border = "1px solid #563440";
     item.appendChild(gridSquare);  
