d4c324ee17c1bdabe716f053071ff3cc6c3d452f
gldsct <188839581+gldsct@users.noreply.github.com>
2024-12-02 11:24:13 +0545

    Add square grids within container

---
+++ javascript.js
@@ -0,0 +1,44 @@
+gridContainer = document.querySelector(".grid-container");
+
+// gridSquare = document.createElement("div");
+// gridSquare.style.width = "35px";
+// gridSquare.style.height = "35px";
+// gridSquare.style.backgroundColor = "#CFCFCD";
+// gridSquare.style.border = "1px solid #563440";
+
+// gridContainer.appendChild(gridSquare);
+
+// for (i = 0; i < 16; i ++) {
+//     gridSquare = document.createElement("div");
+//     gridSquare.style.width = "35px";
+//     gridSquare.style.height = "35px";
+//     gridSquare.style.backgroundColor = "#CFCFCD";
+//     gridSquare.style.border = "1px solid #563440";
+//     gridContainer.appendChild(gridSquare);
+// }
+
+for (i = 0; i < 16; i ++) {
+    gridSemiContainer = document.createElement("div");
+    gridSemiContainer.className = "grid-semi-container";
+    gridSemiContainer.style.height = "35px";
+    gridSemiContainer.style.width = "560px";
+    gridSemiContainer.style.backgroundColor = "#CFCFCD";
+    gridSemiContainer.style.border = "1px solid #563440";
+    gridContainer.appendChild(gridSemiContainer);
+}
+
+gridSemiContainers = document.querySelectorAll(".grid-semi-container");
+gridSemiContainers.forEach(item => {
+    item.style.display = "flex";
+});
+
+gridSemiContainers.forEach(item => {
+    for (i = 0; i < 16; i ++) {
+    gridSquare = document.createElement("div");
+    gridSquare.style.width = "35px";
+    gridSquare.style.height = "35px";
+    gridSquare.style.backgroundColor = "#CFCFCD";
+    gridSquare.style.border = "1px solid #563440";
+    item.appendChild(gridSquare);  
+    }
+});
